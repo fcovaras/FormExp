079869d60f40b5851f4a8721d8f89dffdd800b37
Francisco Varas <44787664+fcovaras@users.noreply.github.com>
2022-07-29 12:59:19 -0400

    Add files via upload

---
+++ WindowsFormsApp1/FormExp.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace FormExp
+{
+    partial class FormExp
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.lblRegistro = new System.Windows.Forms.Label();
+            this.txtNroRegistro = new System.Windows.Forms.TextBox();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.txtTotalRegistro = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(27, 38);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.ReadOnly = true;
+            this.txtArchivo.Size = new System.Drawing.Size(485, 20);
+            this.txtArchivo.TabIndex = 1;
+            // 
+            // lblRegistro
+            // 
+            this.lblRegistro.AutoSize = true;
+            this.lblRegistro.Location = new System.Drawing.Point(24, 73);
+            this.lblRegistro.Name = "lblRegistro";
+            this.lblRegistro.Size = new System.Drawing.Size(46, 13);
+            this.lblRegistro.TabIndex = 2;
+            this.lblRegistro.Text = "Registro";
+            // 
+            // txtNroRegistro
+            // 
+            this.txtNroRegistro.Location = new System.Drawing.Point(76, 70);
+            this.txtNroRegistro.Name = "txtNroRegistro";
+            this.txtNroRegistro.ReadOnly = true;
+            this.txtNroRegistro.Size = new System.Drawing.Size(74, 20);
+            this.txtNroRegistro.TabIndex = 3;
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Location = new System.Drawing.Point(156, 73);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(21, 13);
+            this.lblDe.TabIndex = 4;
+            this.lblDe.Text = "De";
+            // 
+            // txtTotalRegistro
+            // 
+            this.txtTotalRegistro.Location = new System.Drawing.Point(183, 70);
+            this.txtTotalRegistro.Name = "txtTotalRegistro";
+            this.txtTotalRegistro.ReadOnly = true;
+            this.txtTotalRegistro.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalRegistro.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Exportando a Excel...por favor espere.";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.progressBar1.Location = new System.Drawing.Point(27, 103);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(485, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 122);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormExp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 148);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotalRegistro);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.txtNroRegistro);
+            this.Controls.Add(this.lblRegistro);
+            this.Controls.Add(this.txtArchivo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormExp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exportación Excel";
+            this.Load += new System.EventHandler(this.FormExp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Label lblRegistro;
+        private System.Windows.Forms.TextBox txtNroRegistro;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.TextBox txtTotalRegistro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
